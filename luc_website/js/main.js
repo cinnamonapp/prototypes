2a453368191e61a8429ac870fe517c065806ffec
Alessio Santo <alessio.santocs@gmail.com>
2014-07-01 09:42:05 +0200

    Refined luc scrolling

--- luc_website/js/main.js
+++ luc_website/js/main.js
@@ -12,9 +12,20 @@ require(["jquery"], function ($) {
 	require(["lib/vertical-align"]);
 	require(["lib/caret-blink"]);
 	require(["lib/bullets"]);
-	
-	require(["lib/snap.svg-min"], function (Snap) {
-		require(["luc"]);
+	require(["lib/jquery.scrollTo.min"], function(){
+		$('a').click(function (event) {
+			var href = $(this).attr("href");
+
+			if(href.indexOf("#") == 0){
+				event.preventDefault();
+				$.scrollTo($(href), 1000, function () {
+					window.location.hash = href;
+				});
+			}
+
+		});
 	});
+	
+	require(["luc"]);
 
 });
