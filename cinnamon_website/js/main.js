2a2b1125cc98a501989ff4b6199d5e801484e66e
Alessio Santo <alessio.santocs@gmail.com>
2014-07-02 17:30:52 +0200

    Cinnamon is almost over

--- cinnamon_website/js/main.js
+++ cinnamon_website/js/main.js
@@ -1,5 +1,22 @@
-require(["lib/jquery.min"], function (jquery) {
+
+requirejs.config({
+   "paths": {
+     "jquery": "lib/jquery.min",
+     "waypoints": "lib/waypoints"
+   }
+});
+
+
+require(["jquery", "waypoints"], function ($, W) {
 	require(["lib/caret-blink", "lib/bootstrap.min", "lib/window-sized", "lib/vertical-align", "lib/jquery.scrollTo.min", "lib/snap.svg-min"], function (){
 		require(["cinnamon"]);
+
+
+		$("#cinnamon-seed").waypoint(function(direction){
+			if(direction == "down")
+				$(this).removeClass("fadeout");
+			else
+				$(this).addClass("fadeout");
+		});
 	});
 });
