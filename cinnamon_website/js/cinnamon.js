4206ead21939cc34f76c93d61156f5a04d437d07
Alessio Santo <alessio.santocs@gmail.com>
2014-07-08 21:39:53 +0200

    A lot of bug fixed and improvements done

--- cinnamon_website/js/cinnamon.js
+++ cinnamon_website/js/cinnamon.js
@@ -28,43 +28,74 @@ $(document).ready(function () {
 	scrollTo("#cinnamon-seed", 0);
 
 
-	// Flower cmd animation
-	var cinamonSmallPlant = Snap("#SmallFlowers");
-	var cinnamonLittleFlowers = cinamonSmallPlant.selectAll(".ci-animated-flower");
+	// Flower cmd animation ========================
 
-	$(".cmd-flower-animate").on("mouseout", function(){
-		cinnamonLittleFlowers.forEach(function (flower, index) {
+		var cinamonSmallPlant = Snap("#SmallFlowers");
+		var cinnamonLittleFlowers = cinamonSmallPlant.selectAll(".ci-animated-flower");
+		var cinnamonFlowersLinks = $(".cmd-flower-animate");
+		// Hover effect on flowers
+		cinnamonLittleFlowers.forEach(function(flower, index){
+			flower.hover(function (event) {
+				var cmdlinkclass = this.attr("data-cmdlink");
+				cinnamonLittleFlowers.forEach(function (f, i) {
+					f.addClass("ci-flower-blur");
+					cinnamonFlowersLinks.removeClass("hover");
+				});
+
+				this.removeClass("ci-flower-blur");
+
+				$("." + cmdlinkclass).addClass("hover");
+			});
+
+			flower.mouseout(function (event) {
+				cinnamonFlowersLinks.removeClass("hover");
+				cinnamonLittleFlowers.forEach(function (f, i) {
+					f.removeClass("ci-flower-blur");
+				});
+			});
+		});
+
+		cinnamonFlowersLinks.on("mouseout", function(){
+			cinnamonLittleFlowers.forEach(function (flower, index) {
+				flower.removeClass("ci-flower-blur");
+			});
+		});
+		cinnamonFlowersLinks.on("mouseover", function(){
+			var flower_class = $(this).attr("data-flower-class");
+			
+			cinnamonLittleFlowers.forEach(function (flower, index) {
+				flower.addClass("ci-flower-blur");
+				// flower.addClass("ci-flower-stop-animation");
+			});
+
+			var flower = cinamonSmallPlant.select("." + flower_class);
+
 			flower.removeClass("ci-flower-blur");
-			// flower.addClass("ci-flower-stop-animation");
-		});
-	});
-	$(".cmd-flower-animate").on("mouseover", function(){
-		var flower_class = $(this).attr("data-flower-class");
-		
-		cinnamonLittleFlowers.forEach(function (flower, index) {
-			flower.addClass("ci-flower-blur");
-			// flower.addClass("ci-flower-stop-animation");
+			// flower.removeClass("ci-flower-stop-animation");
+
 		});
 
-		var flower = cinamonSmallPlant.select("." + flower_class);
-
-		flower.removeClass("ci-flower-blur");
-		// flower.removeClass("ci-flower-stop-animation");
-
-	});
-
+	// ====================================================================
 
 
 	// cinnaphone carousel
 	var cinnaphones = $("#cinnaphone .ci-presentation-phone");
 
 	window.changeCinnaphoneSlide = function (index) {
+		var element;
+
+		if(index == "next"){
+			element = $("#cinnaphone .ci-presentation-phone.active").next();
+			if(element.length == 0)
+				element = $("#cinnaphone .ci-presentation-phone.first");
+
+			$("#cinnaphone-bullet").bullets("select", element.index() - 1);
+		}else
+			element = $(cinnaphones[index]);
+
 		$(cinnaphones).removeClass("active");
-		$(cinnaphones[index]).addClass("active");
+		element.addClass("active");
 	};
-
-
-
 
 
 	// Custom email form
@@ -86,4 +117,19 @@ $(document).ready(function () {
 		return false;
 	});
 
+
+	$("input[placeholder]").each(function (index) {
+		$(this).val($(this).attr("placeholder"));
+	});
+
+	$("input[placeholder]").on("focus blur",function (event) {
+		if($(this).val() == "")
+			$(this).val($(this).attr("placeholder"));
+
+	});
+
+	$("input[placeholder]").focus(function(){
+		if($(this).val() == $(this).attr("placeholder"))
+			$(this).val("");
+	});
 });
