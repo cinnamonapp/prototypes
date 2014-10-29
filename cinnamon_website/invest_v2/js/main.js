ad416059a0141f144575e1ae77589f6652d842d6
Alessio Santo <alessio.santocs@gmail.com>
2014-10-29 12:53:03 +0100

    Added mailto and fixed bug in js

--- cinnamon_website/invest_v2/js/main.js
+++ cinnamon_website/invest_v2/js/main.js
@@ -34,5 +34,10 @@ require(["jquery", "waypoints"], function ($, W) {
       if($(".navbar-toggle").css("display") !== "none")
         $(".navbar-toggle").click() //bootstrap 3.x by Richard
     });
+
+    $('section').on("touchstart", function() {
+      if($(".navbar-collapse.collapse").hasClass("in"))
+        $(".navbar-toggle").click() //bootstrap 3.x by Richard
+    });
 	});
 });
