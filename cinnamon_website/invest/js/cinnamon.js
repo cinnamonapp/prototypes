315356b829e2ff006934fd1eff42adf56a2fb95c
Alessio Santo <alessio.santocs@gmail.com>
2014-10-31 17:46:51 +0100

    Carousell added and improved a lot with css animations. Now working on video bug

--- cinnamon_website/invest/js/cinnamon.js
+++ cinnamon_website/invest/js/cinnamon.js
@@ -1,25 +1,32 @@
 
 $(document).ready(function () {
 
+  var calculatePercentage = function (video) {
+    var vid = video;
+    var percent = null;
+
+    if (vid.buffered.length > 0 && vid.buffered.end && vid.duration) {
+        percent = vid.buffered.end(0) / vid.duration;
+    } else if (vid.bytesTotal != undefined && vid.bytesTotal > 0 && vid.bufferedBytes != undefined) {
+        percent = vid.bufferedBytes / vid.bytesTotal;
+    }
+
+    return percent;
+
+  };
+
+  console.log($(".show-after-preload").length + " elements");
   $(".show-after-preload").on('progress', function(event) {
-      
-      var vid = this;
-      var percent = null;
+    var percent = calculatePercentage(this);
 
-      if (vid.buffered.length > 0 && vid.buffered.end && vid.duration) {
-          percent = vid.buffered.end(0) / vid.duration;
-      } else if (vid.bytesTotal != undefined && vid.bytesTotal > 0 && vid.bufferedBytes != undefined) {
-          percent = vid.bufferedBytes / vid.bytesTotal;
+    if (percent !== null) {
+      percent = 100 * Math.min(1, Math.max(0, percent));
+
+      if(percent > 80){
+        vid.style.opacity = 1;
+        vid.play();
       }
-
-      if (percent !== null) {
-          percent = 100 * Math.min(1, Math.max(0, percent));
-
-          if(percent > 80){
-            vid.style.opacity = 1;
-            vid.play();
-          }
-      }
+    }
   });
 
 
